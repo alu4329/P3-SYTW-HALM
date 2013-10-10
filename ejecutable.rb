fa65e5296fff9104db79ae3b144209916496d571
Victor Plaza Martín <plazamartin.victor@gmail.com>
2013-10-10 18:12:34 +0100

    Creación y primeras modificaciones de Ejecutable.rb

---
+++ ejecutable.rb
@@ -0,0 +1,62 @@
+require 'rack/request'
+require 'rack/response'
+  
+  module PiedraPapelTijera
+    class App 
+  
+      def initialize(app = nil)
+        @app = app
+        @content_type = :html
+        @defeat = {'piedra' => 'tijeras', 'papel' => 'piedra', 'tijera' => 'papel'}
+        @throws = @defeat.keys
+        @choose = @throws.map { |x| 
+           %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
+        }.join("\n")
+        @choose = "<p>\n<ul>\n#{@choose}\n</ul>"
+      end
+  
+      def call(env)
+        req = Rack::Request.new(env)
+  
+        req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
+  
+        computer_throw = @throws.sample
+        player_throw = req.GET["choice"]
+        answer = if !@throws.include?(player_throw)
+            "Elija una de las siguientes:"
+          elsif player_throw == computer_throw
+            "Empataste con la máquina"
+          elsif computer_throw == @defeat[player_throw]
+            "Felicidades, ganaste; #{player_throw} gana #{computer_throw}"
+          else
+            "Lástima; #{computer_throw} gana #{player_throw}. Suerte la próxima vez"
+          end
+  
+        res = Rack::Response.new
+        res.write <<-"EOS"
+        <html>
+          <title>PiedraPapelTijera</title>
+          <body>
+            <h1>
+               #{answer}
+               #{@choose}
+            </h1>
+          </body>
+        </html>
+        EOS
+        res.finish
+      end # call
+    end   # App
+  end     # PiedraPapelTijera
+  
+  if $0 == __FILE__
+    require 'rack'
+    require 'rack/showexceptions'
+    Rack::Server.start(
+      :app => Rack::ShowExceptions.new(
+                Rack::Lint.new(
+                  PiedraPapelTijera::App.new)), 
+      :Port => 9292,
+      :server => 'thin'
+    )
+  end
